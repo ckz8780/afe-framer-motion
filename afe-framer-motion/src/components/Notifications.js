0702172ef95a148f7a4a4a6b3565b6f0b8e756f2
Chris Z <ckz8780@gmail.com>
2020-11-29 01:12:44 +0000

    Added SVG animation to Notifications slide

--- afe-framer-motion/src/components/Notifications.js
+++ afe-framer-motion/src/components/Notifications.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import { Jumbotron, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
+const pathVariants = {
+  before: {
+    pathLength: 0
+  },
+  after: {
+    pathLength: 1,
+    transition: {
+      duration: 1,
+    }
+  }
+}
 function Notifications() {
   return (
     <Jumbotron fluid className="bg-primary text-light">
       <Container>
         <h1 className="display-1">Notifications</h1>
+        <div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="200"
+            height="130"
+          >
+            <motion.path
+              d="M 5 0 Q 5 50 50 50 Q 100 50 100 95 Q 100 50 150 50 Q 195 50 195 5 Q 100 50 5 2.5"
+              fill="transparent"
+              strokeWidth="5"
+              stroke="#fafafa"
+              initial="before"
+              animate="after"
+              variants={pathVariants}
+            />
+          </svg>
+        </div>
         <Link to="/icons">
           <Button variant="outline-light">Continue</Button>
         </Link>
